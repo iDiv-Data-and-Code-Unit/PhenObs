2cef1770ebf9cf91f6ce0dcc76e7f02f7e995418
zxyctn <zxyctn@gmail.com>
2022-02-25 11:33:52 +0100

    Closes #43; Add new garden and subgardens

--- phenobs/gardens/models.py
+++ phenobs/gardens/models.py
@@ -16,7 +16,7 @@ class Garden(models.Model):
 
     """
 
-    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=100, blank=False, null=False)
     latitude = models.DecimalField(
         max_digits=9, decimal_places=6, blank=False, help_text="WGS 84"
     )
