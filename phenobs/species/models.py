1cb92bd36de106c9cb6ded2effbd7c3d7fdcd8ad
zxyctn <zxyctn@gmail.com>
2022-06-08 17:59:34 +0200

    Make species reference_name unique

--- phenobs/species/models.py
+++ phenobs/species/models.py
@@ -10,7 +10,7 @@ class Species(models.Model):
 
     """
 
-    reference_name = models.CharField(max_length=100)
+    reference_name = models.CharField(max_length=100, unique=True)
     reference_id = models.IntegerField(blank=True, null=True)
 
     def __str__(self) -> str:
