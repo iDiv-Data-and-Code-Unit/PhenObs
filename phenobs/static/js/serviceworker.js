3b053e121385c291b0cf52a8e070c6fedb6c8c47
zxyctn <zxyctn@gmail.com>
2022-08-30 12:16:48 +0200

    Fix caching issues with serviceworker

--- phenobs/static/js/serviceworker.js
+++ phenobs/static/js/serviceworker.js
@@ -1,11 +1,11 @@
 var today = new Date();
-const staticCacheName = "django-pwa-v112";
+const staticCacheName = "django-pwa-v117";
 var filesToCache = [
     '/observations/',
     '/offline/',
     '/imprint/',
     '/help/',
-    '/static/css/project.css',
+
     '/static/css/bootstrap.min.css',
     '/static/js/bootstrap.min.js',
     '/static/js/html5shiv.min.js',
@@ -19,13 +19,8 @@ var filesToCache = [
     '/static/js/spreadsheet.js',
     '/static/js/collection.js',
     '/static/js/modals.js',
-    '/static/images/PhenObs_Logo.png',
-    '/static/images/PhenObs_Splash.png',
-    '/static/images/PhenObs_Logo_Rounded_512px.png',
-    '/static/images/all_collections.png',
-    '/static/images/add_collection.png',
-    '/static/images/add_observation.png',
-    '/static/images/loading.gif',
+    '/static/js/help.js',
+
     '/static/css/bootstrap-icons.scss',
     '/static/css/bootstrap-icons.css',
     '/static/fonts/bootstrap-icons.woff',
@@ -33,15 +28,19 @@ var filesToCache = [
     '/static/fonts/bootstrap-icons.woff?a74547b2f0863226942ff8ded57db345',
     '/static/fonts/bootstrap-icons.woff2?a74547b2f0863226942ff8ded57db345',
 
-    '/static/images/bootstrap-icons/cloud-arrow-up-fill.svg',
-    '/static/images/bootstrap-icons/cloud-check-fill.svg',
-    '/static/images/bootstrap-icons/cloud-fill.svg',
-    '/static/images/bootstrap-icons/exclamation-circle-fill.svg',
-    '/static/images/bootstrap-icons/hdd-fill.svg',
-    '/static/images/bootstrap-icons/pencil-fill.svg',
-    '/static/images/bootstrap-icons/trash-fill.svg',
-    '/static/images/bootstrap-icons/filetype-csv.svg',
-    '/static/images/bootstrap-icons/plus-circle-fill.svg',
+    '/static/css/help.css',
+    '/static/css/project.css',
+    '/static/sass/project.scss',
+
+    '/static/CSV_template.csv',
+
+    '/static/images/PhenObs_Logo.png',
+    '/static/images/PhenObs_Splash.png',
+    '/static/images/PhenObs_Logo_Rounded_512px.png',
+    '/static/images/all_collections.png',
+    '/static/images/add_collection.png',
+    '/static/images/add_observation.png',
+    '/static/images/loading.gif',
 
     '/static/images/save_db_primary.png',
     '/static/images/db_check_success.png',
