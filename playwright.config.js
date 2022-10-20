e9eb51099fa3fa0c113a5a2d8b7a7bf33b2ab8a7
zxyctn <zxyctn@gmail.com>
2022-10-20 22:52:39 +0200

    Add E2E tests for Help page

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ const config = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
-  retries: 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined,
   workers: 1,
