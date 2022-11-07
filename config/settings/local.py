ef8d132ec4d75980e9c8139cdb2e18db2d3861c5
zxyctn <zxyctn@gmail.com>
2022-11-07 16:12:47 +0100

    Remove default secret key

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,6 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="xk7t5uB2rw1QZKKmCaIvSRFIQP8ll39kovBpHnxRy899JV7M0F4HzRPqq72uqOJT",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", ".loca.lt"]
